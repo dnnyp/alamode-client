d4f07b3794639d2d15af6f9b5d274d8699d15bb3
dnnyp <dnnypham@gmail.com>
2019-10-18 01:17:28 -0400

    Styling: Add responsive styling to report header
    - make edit and delete buttons in-line with header
    - responsively size header text size and container width
    - use ellipsis for overflow

--- src/components/Reports/ReportControls.js
+++ src/components/Reports/ReportControls.js
@@ -2,26 +2,44 @@ import React, { Fragment } from 'react'
 import Button from 'react-bootstrap/Button'
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
 
+const styles = {
+  header: {
+    display: 'block',
+    textOverflow: 'ellipsis',
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+    fontSize: 'calc(1em + 1vw)'
+  }
+}
+
 const ReportControls = props => {
   return (
     <Fragment>
-      <a rel="noopener noreferrer" href={props.url} target="_blank"><h3>{props.title}</h3></a>
-      {props.owner === props.user &&
-        <ButtonGroup size="sm" className="mt-3">
-          <Button
-            variant="outline-secondary"
-            onClick={props.onClick}
-          >
-            Edit Title
-          </Button>
-          <Button
-            variant="outline-secondary"
-            onClick={props.onDelete}
-          >
-            Delete Report
-          </Button>
-        </ButtonGroup>
-      }
+      <div className="container">
+        <div className="row justify-content-between">
+          <div className="col-7 col-md-10 p-0 m-0">
+            <a rel="noopener noreferrer" href={props.url} target="_blank" style={styles.header}>{props.title}</a>
+          </div>
+          <div className="col-auto justify-content-end col-sm-4 col-md-2 d-flex p-0 m-0">
+            {props.owner === props.user &&
+              <ButtonGroup>
+                <Button
+                  variant="outline-secondary"
+                  onClick={props.onClick}
+                >
+                  Edit
+                </Button>
+                <Button
+                  variant="outline-secondary"
+                  onClick={props.onDelete}
+                >
+                  Delete
+                </Button>
+              </ButtonGroup>
+            }
+          </div>
+        </div>
+      </div>
     </Fragment>
   )
 }
