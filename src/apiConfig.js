57b0da355a2354aadaaacf6b5566f70d4be861cd
dnnyp <dnnypham@gmail.com>
2020-01-16 17:28:36 -0500

    Refactor: Update dev API URL
    - change port to 5000 to reflect change in dev API's port

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
   production: 'https://quiet-thicket-71875.herokuapp.com',
-  development: 'http://localhost:4741'
+  development: 'http://localhost:5000'
 }
 
 if (window.location.hostname === 'localhost') {
