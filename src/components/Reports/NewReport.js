85d29c906af2acf3f97f4e2a223369d57e93ad74
dnnyp <dnnypham@gmail.com>
2020-02-04 23:43:15 -0500

    Feature: Show progress bar for report creation
    - add a "progress" state to NewReport component
    - add a socket listener for job progress events (job ID + suffix '-p')
      - update "progress" with the data from the event
    - add Bootstrap progress bar which uses "progress" as the current value
    - add a '-c' suffix to the event listener for completed job events to
      distinguish from job progress events
      - set "progress" to 100 upon job completion

--- src/components/Reports/NewReport.js
+++ src/components/Reports/NewReport.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 
+import ProgressBar from 'react-bootstrap/ProgressBar'
+
 import messages from '../AutoDismissAlert/messages'
 
 const apiUrl = require('./../../apiConfig').default
@@ -8,10 +10,22 @@ const io = require('socket.io-client')
 const socket = io(apiUrl)
 
 class NewReport extends Component {
+  constructor () {
+    super()
+    this.state = {
+      progress: 0
+    }
+  }
+
   componentDidMount () {
     const { alert, match, history } = this.props
 
-    socket.on(match.params.jobId, data => {
+    socket.on(match.params.jobId + '-p', data => {
+      this.setState({ progress: data.progress })
+    })
+
+    socket.on(match.params.jobId + '-c', data => {
+      this.setState({ progress: 100 })
       alert({
         heading: 'Create Report Success',
         message: messages.createReportSuccess,
@@ -27,6 +41,8 @@ class NewReport extends Component {
         <div className="col-sm-10 col-md-8 mx-auto mt-5">
           <h3>Generating Report</h3>
           <p>Please do not refresh the page and wait while we are preparing your report.</p>
+
+          <ProgressBar animated now={this.state.progress} />
         </div>
       </div>
     )
